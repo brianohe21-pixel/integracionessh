0a5891681b0f44e1eebfda6fa0087a6ec9240b12
Brayan <brianohe21@gmail.com>
2026-08-03 14:18:30 -0500

    feat(lambda): add reseller log group to variable exclusions
    - Added "reseller" to the list of Lambda keys whose log groups are created by Terraform instead of imported, enhancing log management for new reseller functions.

--- infrastructure/modules/lambda/variables.tf
+++ infrastructure/modules/lambda/variables.tf
@@ -288,6 +288,7 @@ variable "cloudwatch_log_group_import_exclude" {
     "reports",
     "voicebot",
     "voicebot_session",
+    "reseller",
   ]
   description = "Lambda keys whose log groups are created by Terraform instead of imported (new functions without pre-existing log groups in AWS)"
 }
